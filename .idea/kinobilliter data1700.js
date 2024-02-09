55a0e8ca770162719ff615f0d753707de6361901
wakre <wakre6980@oslomet.no>
2024-02-09 16:40:26 +0100

    index.html

---
+++ .idea/kinobilliter data1700.js
@@ -0,0 +1,7 @@
+ut+="<br/>Velg Film: "+document.getElementById("film").value;
+document.getElementById("resultat").innerHTML=ut;
+ type="application/javascript"
+document.getElementById("bmi").innerText=navn+" har BMI =  "+bmi.toFixed(2);
+
+
+
