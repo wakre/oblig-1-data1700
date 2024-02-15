80ff4a7218df9d5cc9b998c29b4964689f319d01
wakre <wakre6980@oslomet.no>
2024-02-15 14:43:00 +0100

    javascript og html side

--- .idea/kinobilliter data1700.js
+++ .idea/kinobilliter data1700.js
@@ -12,6 +12,58 @@ function addToArray() {
     populateHTML(filmArray);
 
 }
+function validerAntall(antall){
+    const regexp = /^[0-9]{1,2}$/;
+    const ok = regexp.test(antall);
+    if(!ok){
+        $("#feilAntall").html("Velg minst 1, og maks  2 siffer");
+        return false;
+    }else{
+        $("#feilAntall").html("");
+    }
+}
+function validerFornavn(fornavn){
+    const regexp = /^[a-zA-ZæøåÆØÅ.\-]{2,20}$/;
+    const ok = regexp.test(fornavn);
+    if(!ok){
+        $("#feilFornavn").html("Må skrive noe gyldig i Fornavnet");
+        return false;
+    }else{
+        $("#feilFornavn").html("");
+    }
+}
+
+function validerEtternavn(etternavn){
+    const regexp = /^[a-zA-ZæøåÆØÅ.\-]{2,20}$/;
+    const ok = regexp.test(etternavn);
+    if(!ok){
+        $("#feilEtternavn").html("Må skrive noe gyldig i Etternavn");
+        return false;
+    }else{
+        $("#feilEtternavn").html("");
+    }
+}
+function validerTelefonnr(telefonnr){
+    const regexp = /^[0-9. \-]{8}$/;
+    const ok = regexp.test(telefonnr);
+    if(!ok){
+        $("#feilTelefonnr").html("Må skrive noe gyldig inni Telefonnr");
+        return false;
+    }else{
+        $("#feilTelefonnr").html("");
+    }
+}
+function validerEpost(epost){
+    const regexp = /^[a-zA-Z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z\-]{2,}$/;
+    const ok = regexp.test(epost);
+    if(!ok){
+        $("#feilEpost").html("Må skrive noe gyldig i Eposten");
+        return false;
+    }else{
+        $("#feilEpost").html("");
+    }
+}
+
 function populateHTML(objArr){
     console.log("film")
     let html = "<ol>";
@@ -24,6 +76,7 @@ function populateHTML(objArr){
     document.getElementById("kjøpte billetter").innerHTML = html;
     console.log(html)
 }
+
 function tomArray(){
     filmArray=[];
     populateHTML(filmArray);
